7a4ee6e1379051753331ce47ef908f5b3dedd31e
oneflybird <1361569005@qq.com>
2019-05-12 17:01:07 +0800

    增加了查看的方式

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import ipfsapi
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, request
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_wtf import Form
@@ -14,15 +14,28 @@ class NameForm(Form):
 def getdata():
     api = ipfsapi.connect('129.211.27.244', 5001)
     form = NameForm()
+    hash_key = ''
     if form.validate_on_submit():
         with open('cont.txt','w') as f:
             session['words'] = form.words.data
+            print(form.words.data)
             f.write(form.words.data)
-        api.add('cont.txt')
+        hash_key = api.add('cont.txt')['Hash']
         with open('cont.txt','w') as f:
             f.write("")
-        return redirect(url_for('getdata'))
-    return render_template('ipfs.html', form=form, words=session.get('words'))
+    return render_template('ipfs.html', form=form, words=session.get('words'),hash_key = hash_key)
+
+
+@app.route('/news',methods=['GET', 'POST'])
+def news():
+    new = ''
+    api = ipfsapi.connect('129.211.27.244', 5001)
+    if request.method == 'POST':
+        hash_key = request.form['hash_key']
+        print(hash_key)
+        new = api.cat(hash_key).decode('gbk')
+        print(new)
+    return render_template('news.html',new = new)
 
 
 if __name__ == '__main__':
--- bytom/ipfs.py
+++ bytom/ipfs.py
@@ -1,13 +1,18 @@
 import ipfsapi
 
 # 连接IPFS，需要先启动节点服务器daemon
-api = ipfsapi.connect('129.211.27.244', 5001)
+# api = ipfsapi.connect('129.211.27.244', 5001)
 
 # 查看节点ID
 # print(api.id())
-res = api.add('test.txt')
+# res = api.add('test.txt')
 
 # 查看文件内容
-# content = api.cat(res['Hash'])
+if __name__ == '__main__':
+    api = ipfsapi.connect('129.211.27.244', 5001)
+    # res = api.add('test.txt')
+    content = api.cat('QmPFDCuavg58zyQVdDHVgUqos2CVSmDTLa1cRZESK8qARZ')
+    print(content)
+    print(content.decode('gbk'))
 # 下载文件
 # res = api.get(res['Hash'])
